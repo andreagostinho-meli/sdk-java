69e7ce8ccc80b6f3f438f6e5c84f984068cd5fa2
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-12-05 17:06:51 -0300

    test: add create order integration test

--- src/test/java/com/mercadopago/client/order/OrderClientIT.java
+++ src/test/java/com/mercadopago/client/order/OrderClientIT.java
@@ -1,5 +1,6 @@
 package com.mercadopago.client.order;
 
+import com.mercadopago.BaseClientIT;
 import com.mercadopago.client.cardtoken.CardTokenTestClient;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
@@ -15,38 +16,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class OrderClientIT {
+/** OrderClientIT class. */
+public class OrderClientIT extends BaseClientIT {
   private final OrderClient client = new OrderClient();
-
   private final CardTokenTestClient cardTokenTestClient = new CardTokenTestClient();
 
   @Test
   public void createSuccess() {
     try {
       CardToken cardToken = cardTokenTestClient.createTestCardToken("approved");
-
-
-      List<OrderPaymentRequest> paymentRequest = new ArrayList<OrderPaymentRequest>();
-      paymentRequest.add(
-          OrderPaymentRequest.builder()
-              .amount("10.00")
-              .currency("BRL")
-              .paymentMethod(
-                  OrderPaymentMethodRequest.builder()
+      List<OrderPaymentRequest> paymentRequest = new ArrayList<>();
+      paymentRequest.add(OrderPaymentRequest.builder()
+              .amount("100.00")
+              .paymentMethod(OrderPaymentMethodRequest.builder()
                       .id("master")
                       .type("credit_card")
                       .token(cardToken.getId())
                       .installments(1)
-                  .build())
-          .build());
-
-      OrderCreateRequest orderCreateRequest =
-          OrderCreateRequest.builder()
-              .totalAmount("10.00")
-              .description("test")
+                      .build())
+              .build());
+      OrderPayerRequest orderPayerRequest = OrderPayerRequest.builder()
+              .email("test_1731350184@testuser.com")
+              .build();
+      OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
+              .type("online")
+              .totalAmount("100.00")
+              .processingMode("automatic")
+              .externalReference("ext_ref_1234")
+              .payer(orderPayerRequest)
               .transactions(OrderTransactionRequest.builder()
-                  .payments(paymentRequest)
-                  .build())
+                      .payments(paymentRequest)
+                      .build())
               .build();
 
       Order order = client.create(orderCreateRequest);
@@ -54,11 +54,14 @@ public class OrderClientIT {
       assertNotNull(order.getResponse());
       assertEquals(CREATED, order.getResponse().getStatusCode());
       assertNotNull(order.getId());
+      assertEquals("100.00", order.getTotalAmount());
+      assertEquals("master", order.getTransactions().getPayments().get(0).getPaymentMethod().getId());
+      assertEquals("credit_card", order.getTransactions().getPayments().get(0).getPaymentMethod().getType());
+      assertEquals(1, order.getTransactions().getPayments().get(0).getPaymentMethod().getInstallments());
     } catch (MPApiException mpApiException) {
       fail(mpApiException.getApiResponse().getContent());
     } catch (MPException mpException) {
       fail(mpException.getMessage());
     }
   }
-
 }
