8165d828588a63b3c0f6f6f6c79a4ad11c365cad
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-12-09 16:22:55 -0300

    test: cancel order

--- src/test/java/com/mercadopago/client/order/OrderClientIT.java
+++ src/test/java/com/mercadopago/client/order/OrderClientIT.java
@@ -197,4 +197,48 @@ public class OrderClientIT extends BaseClientIT {
       fail(mpException.getMessage());
     }
   }
+
+  @Test
+  public void cancelOrderSuccess() {
+    try {
+      CardToken cardToken = cardTokenTestClient.createTestCardToken("approved");
+      List<OrderPaymentRequest> paymentRequest = new ArrayList<>();
+      paymentRequest.add(OrderPaymentRequest.builder()
+              .amount("100.00")
+              .paymentMethod(OrderPaymentMethodRequest.builder()
+                      .id("master")
+                      .type("credit_card")
+                      .token(cardToken.getId())
+                      .installments(1)
+                      .build())
+              .build());
+      OrderPayerRequest orderPayerRequest = OrderPayerRequest.builder()
+              .email("test_1731350184@testuser.com")
+              .build();
+      OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
+              .type("online")
+              .totalAmount("100.00")
+              .processingMode("manual")
+              .externalReference("ext_ref_1234")
+              .payer(orderPayerRequest)
+              .transactions(OrderTransactionRequest.builder()
+                      .payments(paymentRequest)
+                      .build())
+              .build();
+
+      Order order = client.create(orderCreateRequest);
+      Order cancelledOrder = client.cancel(order.getId());
+
+      assertNotNull(cancelledOrder.getResponse());
+      assertEquals(OK, cancelledOrder.getResponse().getStatusCode());
+      assertEquals(order.getId(), cancelledOrder.getId());
+      assertEquals("cancelled", cancelledOrder.getStatus());
+      assertEquals("cancelled", cancelledOrder.getTransactions().getPayments().get(0).getStatus());
+      assertEquals("cancelled_transaction", cancelledOrder.getTransactions().getPayments().get(0).getStatusDetail());
+    } catch (MPApiException mpApiException) {
+      fail(mpApiException.getApiResponse().getContent());
+    } catch (MPException mpException) {
+      fail(mpException.getMessage());
+    }
+  }
 }
