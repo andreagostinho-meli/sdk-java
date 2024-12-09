405b24e34e4d944a63924561df0f09f3c89106c3
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-12-09 18:51:14 -0300

    test: delete Order transaction

--- src/test/java/com/mercadopago/client/order/OrderClientIT.java
+++ src/test/java/com/mercadopago/client/order/OrderClientIT.java
@@ -2,6 +2,7 @@ package com.mercadopago.client.order;
 
 import com.mercadopago.BaseClientIT;
 import com.mercadopago.client.cardtoken.CardTokenTestClient;
+import com.mercadopago.core.MPRequestOptions;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.CardToken;
@@ -11,11 +12,9 @@ import com.mercadopago.resources.order.OrderTransaction;
 import com.mercadopago.resources.order.UpdateOrderTransaction;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static com.mercadopago.net.HttpStatus.CREATED;
-import static com.mercadopago.net.HttpStatus.OK;
+import static com.mercadopago.net.HttpStatus.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -430,6 +429,52 @@ public class OrderClientIT extends BaseClientIT {
       assertNotNull(updatedTransaction.getResponse());
       assertEquals(OK, updatedTransaction.getResponse().getStatusCode());
       assertEquals(3, updatedTransaction.getPaymentMethod().getInstallments());
+    } catch (MPApiException mpApiException) {
+      fail(mpApiException.getApiResponse().getContent());
+    } catch (MPException mpException) {
+      fail(mpException.getMessage());
+    }
+  }
+
+  @Test
+  public void deleteOrderTransactionSuccess() {
+    try {
+      CardToken cardToken = cardTokenTestClient.createTestCardToken("approved");
+      List<OrderPaymentRequest> paymentRequest = new ArrayList<>();
+      paymentRequest.add(OrderPaymentRequest.builder()
+              .amount("100.00")
+              .paymentMethod(OrderPaymentMethodRequest.builder()
+                      .id("master")
+                      .type("credit_card")
+                      .token(cardToken.getId())
+                      .installments(1)
+                      .build())
+              .build());
+      OrderPayerRequest orderPayerRequest = OrderPayerRequest.builder()
+              .email("test_1731350184@testuser.com")
+              .build();
+      OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
+              .type("online")
+              .totalAmount("100.00")
+              .processingMode("manual")
+              .externalReference("ext_ref_1234")
+              .payer(orderPayerRequest)
+              .transactions(OrderTransactionRequest.builder()
+                      .payments(paymentRequest)
+                      .build())
+              .build();
+      MPRequestOptions requestOptions = MPRequestOptions.builder().build();
+      Map<String, String> headers =  new HashMap<>();
+      headers.put("X-Idempotency-Key", UUID.randomUUID().toString());
+      requestOptions.setCustomHeaders(headers);
+
+      Order order = client.create(orderCreateRequest);
+      String orderId = order.getId();
+      String transactionId = order.getTransactions().getPayments().get(0).getId();
+      OrderTransaction deletedTransaction = client.deleteTransaction(orderId, transactionId, requestOptions);
+
+      assertNotNull(deletedTransaction.getResponse());
+      assertEquals(NO_CONTENT, deletedTransaction.getResponse().getStatusCode());
     } catch (MPApiException mpApiException) {
       fail(mpApiException.getApiResponse().getContent());
     } catch (MPException mpException) {
